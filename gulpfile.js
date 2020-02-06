c57c74b63936d69574f2700246e0bc24c923eb05
mtbotherd <mtbotherd@users.noreply.github.com>
2020-02-06 14:01:55 -0600

    Removed unused packages, added Babel, updated gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,144 +1,116 @@
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create(),
-    cache = require('gulp-cache'),
-    del = require('del'),
-    imagemin = require('gulp-imagemin'),
-    runSequence = require('run-sequence'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps')
+var gulp = require("gulp"),
+  autoprefixer = require("gulp-autoprefixer"),
+  browserSync = require("browser-sync").create(),
+  cache = require("gulp-cache"),
+  del = require("del"),
+  runSequence = require("run-sequence"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  cp = require("child_process");
 
 // Development Tasks
 // -----------------
 
 // Start browserSync server
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ['src']
-        }
-    })
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: ["dist"]
+    }
+  });
 });
 
 // Copy vemdor js to src
-gulp.task('vendorjs', function() {
-    return gulp.src([
-            'node_modules/jquery/dist/jquery.js',
-            'node_modules/popper.js/dist/umd/popper.js',
-            'node_modules/bootstrap/dist/js/bootstrap.js',
-            'node_modules/exlink/jquery.exlink.js',
-            'node_modules/svgxuse/svgxuse.js'
-        ])
-        .pipe(gulp.dest('src/template/js'))
+gulp.task("vendorjs", function() {
+  return gulp
+    .src([
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/popper.js/dist/umd/popper.js",
+      "node_modules/bootstrap/dist/js/bootstrap.js",
+      "src/js/vendor/*.js"
+    ])
+    .pipe(gulp.dest("dist/template/js"));
 });
 
 // Compile sass to css
-gulp.task('sass', function() {
-    return gulp.src('src/template/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write('maps'))
-        // .pipe(gulp.dest('src/Data/sites/1/skins/MetroTransitIII'))
-        .pipe(gulp.dest('src/template/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+gulp.task("sass", function() {
+  return gulp
+    .src("src/template/scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("maps"))
+    .pipe(gulp.dest("dist/template/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
 // Copy js to dist
-gulp.task('scripts', function() {
-    return gulp.src([
-            'src/template/js/**/*.js'
-        ])
-        .pipe(gulp.dest('dist/template/js'))
+gulp.task("scripts", function() {
+  return gulp
+    .src(["src/template/js/**/*.js"])
+    .pipe(gulp.dest("dist/template/js"));
 });
 
-// Copy css to dist
-gulp.task('css', function() {
-    return gulp.src([
-            'src/template/css/**/*.css'
-        ])
-        // .pipe(gulp.dest('dist/Data/sites/1/skins/MetroTransitIII'))
-        .pipe(gulp.dest('dist/template/css'))
+// Transpile the project js files to dist using babel
+gulp.task("transpile", function() {
+  return cp.exec("npx babel src/template/js/*.js --out-dir dist/template/js");
 });
-
-// Copy sourcemaps to dist
-// gulp.task('sourcemaps', function() {
-//     return gulp.src('src/maps/**/*.map')
-//         .pipe(gulp.dest('dist/maps'))
-// });
 
 // Copy fonts to dist
-gulp.task('fonts', function() {
-    return gulp.src('src/template/fonts/*')
-        .pipe(gulp.dest('dist/template/fonts'))
+gulp.task("fonts", function() {
+  return gulp
+    .src("src/template/fonts/*")
+    .pipe(gulp.dest("dist/template/fonts"));
 });
 
-gulp.task('html', function () {
-	return gulp.src('src/**/*.html')
-		.pipe(gulp.dest('dist'))
+gulp.task("html", function() {
+  return gulp.src("src/**/*.html").pipe(gulp.dest("dist"));
 });
-
 
 // Watchers
-gulp.task('watch', function() {
-    gulp.watch('src/template/scss/**/*.scss', ['sass']);
-    // gulp.watch('src/Data/sites/1/skins/MetroTransitIII/*.html', browserSync.reload);
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/template/js/**/*.js', browserSync.reload);
+gulp.task("watch", function() {
+  gulp.watch("src/template/scss/**/*.scss", ["sass"]);
+  gulp.watch("src/**/*.html", ["html"]);
+  gulp.watch("src/template/js/**/*.js", ["transpile"]);
+  gulp.watch("dist/**/*").on("change", function() {
+    browserSync.reload();
+  });
 });
-
-// Optimization Tasks
-// ------------------
 
 // Optimize images
-gulp.task('images', function() {
-    return gulp.src('src/template/img/**/*.+(png|jpg|gif|svg)')
-        .pipe(imagemin({
-            interlaced: true
-        })) // refer to https://github.com/sindresorhus/gulp-imagemin for optimization options available based on file type.
-        .pipe(gulp.dest('dist/template/img'))
+gulp.task("images", function() {
+  return gulp
+    .src("src/template/img/**/*.+(png|jpg|gif|svg)")
+    .pipe(gulp.dest("dist/template/img"));
 });
 
-// Optimize CSS and JS
-// gulp.task('useref', function() {
-//     return gulp.src([
-//             // 'src/Data/sites/1/skins/MetroTransitIII/*.html',
-// 			'src/*.html',
-// 			'!src/_primary-boilerplate.html',
-// 			'!src/_secondary-boilerplate.html'
-//         ]) // Grabs CSS and JS from HTML document
-//         .pipe(useref())
-//         .pipe(gulpIf('template/js/*.js', uglify())) // Minifies only if it's a js file
-//         .pipe(gulpIf('template/css/*.css', cssnano())) // Minifies only if it's a css file
-//         // .pipe(gulp.dest('dist/Data/sites/1/skins/MetroTransitIII'))
-//         .pipe(gulp.dest('dist'))
-// });
-
 // Clean Dist
-gulp.task('clean', function() {
-    return del.sync('dist').then(function(cb) {
-        return cache.clearAll(cb);
-    });
-})
+gulp.task("clean", function() {
+  return del.sync("dist").then(function(cb) {
+    return cache.clearAll(cb);
+  });
+});
 
-gulp.task('clean:dist', function() {
-    return del.sync('dist/**/*');
+gulp.task("clean:dist", function() {
+  return del.sync("dist/**/*");
 });
 
 // Build Sequence
 // --------------
-gulp.task('default', function(callback) {
-    runSequence(['vendorjs', 'sass', 'browserSync'], 'watch',
-        callback
-    )
+gulp.task("default", function(callback) {
+  runSequence(["vendorjs", "sass", "html", "browserSync"], "watch", callback);
 });
 
-gulp.task('build', function(callback) {
-    runSequence(
-        'clean:dist',
-        'sass', ['html', 'css', 'fonts', 'scripts', 'images'],
-        callback
-    )
+gulp.task("build", function(callback) {
+  runSequence(
+    "clean:dist",
+    "vendorjs",
+    "sass",
+    ["html", "fonts", "scripts", "transpile", "images"],
+    callback
+  );
 });
