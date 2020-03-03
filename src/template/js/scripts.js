996718bd4d448809de019c455712c5d0d6123578
Rich Bothe <rich.bothe@metrotransit.org>
2020-03-03 17:44:29 -0600

    Removed unused code.

--- src/template/js/scripts.js
+++ src/template/js/scripts.js
@@ -207,32 +207,3 @@ function googleTranslateElementInit() {
         "google_translate_element"
     );
 }
-
-// $(document).ready(function() {
-
-//     'use strict';
-
-//     $('#header img.active').hide();
-
-//     var navImg = $('#header .nav-item');
-//     $(navImg).hover(function() {
-//         $(this).find('img.inactive').hide();
-//         $(this).find('img.active').show();
-//     }, function() {
-//         $(this).find('img.inactive').show();
-//         $(this).find('img.active').hide();
-//     });
-
-// 	// Initialize tooltips
-// 	$('[data-toggle="popover"]').popover();
-
-// 	// Show/hide shipping address
-// 	$("#saShippingAddress").change(function() {
-// 		if(this.checked) {
-// 			$(".shipping-info").fadeOut();
-// 		} else {
-// 			$(".shipping-info").fadeIn();
-// 		}
-// 	});
-
-// });
